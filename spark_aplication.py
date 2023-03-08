1d26d472d29afc7de8b9ad610e1d9474518de508
Miriam <m.aumatell33@gmail.com>
2023-03-08 16:34:41 +0100

    added unit tests to the class

--- spark_aplication.py
+++ spark_aplication.py
@@ -1,101 +1,52 @@
+from typing import List
+from pyspark.sql import DataFrame
 from pyspark.sql import functions as F
 from pyspark.sql import types as T
 from pyspark.sql import Window
 from pyspark.sql import SparkSession
 
-spark = (SparkSession
-         .builder
-         .getOrCreate())
 
-def stores_v1(spark, file_path):
-    stores_schema =  T.StructType([
-        T.StructField("store_id",T.IntegerType(),True), 
-        T.StructField("country",T.StringType(),True), 
-        T.StructField("version",T.StringType(),True), 
-        ])
-    return spark.read.schema(stores_schema).option("header", True).csv(file_path)
+class Transformation:
 
-def stores_v2(spark, file_path):
-    stores_v2_schema =  T.StructType([
-        T.StructField("store_id",T.StringType(),True), 
-        T.StructField("version",T.StringType(),True), 
-        ])
-    return spark.read.schema(stores_v2_schema).option("header", True).csv(file_path)
+    def __init__(self) -> None:
+        self.spark = (SparkSession
+                .builder
+                .getOrCreate())
 
-def stores(spark):
-    stores_v1_file_path = "stores.csv"
-    stores_v2_file_path = "stores_v2.csv"
-    stores_d1 = stores_v1(spark, stores_v1_file_path)
-    stores_d2 = (stores_v2(spark, stores_v2_file_path)
-                 .withColumn("country", F.substring(F.col("store_id"), 1,2))
-                 .withColumn("store_id", F.substring(F.col("store_id"), 3, 4))
-                 .select("store_id", "country", "version")
-                 )
-    return stores_d1.union(stores_d2)
+    def lp_stores(self, stores: DataFrame) -> List:
+        stores_integrated = stores.withColumn("store_id", F.when(
+            F.col("version")==2, F.substring(F.col("store_id"), 3, 4)
+            ).otherwise(F.col("store_id")))
+        return [int(x["store_id"]) for x in (stores_integrated.select("store_id").collect())]
 
-def ticket_lines(spark, file_path):
-    ticket_lines_schema =  T.StructType([
-        T.StructField("ticket_id",T.IntegerType(),True), 
-        T.StructField("product_id",T.IntegerType(),True), 
-        T.StructField("store_id",T.IntegerType(),True), 
-        T.StructField("date",T.DateType(),True), 
-        T.StructField("quantity",T.IntegerType(),True), 
-        ])
-    return spark.read.schema(ticket_lines_schema).option("header", True).csv(file_path)
+    def total_stores_per_product(self, ticket_lines: DataFrame, stores: DataFrame) -> DataFrame:
+        return (ticket_lines
+                .filter(F.col("store_id").isin(self.lp_stores(stores)))
+                .groupBy("product_id")
+                .agg(F.countDistinct("store_id").cast(T.IntegerType()).alias("n_stores"))
+                )
 
-def products(spark, file_path):
-    products_schema = T.StructType([
-        T.StructField("product_id",T.IntegerType(),True), 
-        T.StructField("product_name",T.StringType(),True),
-        T.StructField("categories",T.ArrayType(T.StructType([
-            T.StructField("category_id", T.IntegerType(), True),
-            T.StructField("category_name", T.StringType(), True)
-            ])),True),
-        ])
-    # return spark.createDataFrame(file_path, schema = products_schema)
-    return spark.read.schema(products_schema).json(file_path)
+    def second_most_selling_store_per_product(self, ticket_lines: DataFrame, stores: DataFrame) -> DataFrame:
+        sorted_quantities = Window.partitionBy("product_id").orderBy(F.col('quantity').desc())
+        return (ticket_lines
+                .filter(F.col("store_id").isin(self.lp_stores(stores)))
+                .groupBy("product_id", "store_id")
+                .agg(F.sum("quantity").alias("quantity"))
+                .withColumn("second_quantity", F.collect_list(F.col("quantity")).over(sorted_quantities)[1])
+                .filter(F.col("second_quantity")==F.col("quantity"))
+                .select(F.col("product_id"), F.col("store_id").alias("second_store_id"))
+                )
 
-def lp_stores(spark):
-    return [x["store_id"] for x in (stores(spark)
-                 .select("store_id").collect())]
+    def second_popular_stores_by_product_category(self, ticket_lines: DataFrame, products: DataFrame, stores: DataFrame):
+        return (self.second_most_selling_store_per_product(ticket_lines, stores)
+                .join(products, on="product_id")
+                .select( F.explode(F.col("categories")).alias("category"), "product_id", "product_name", "second_store_id")
+                .groupBy(F.col("category.category_name").alias("category_name"))
+                .agg(F.collect_set("second_store_id").alias("stores"))
+                )
 
-def n_stores_per_product(spark):
-    ticket_lines_file_path = "ticket_line.csv"
-    return (ticket_lines(spark, ticket_lines_file_path) 
-            .filter(F.col("store_id").isin(lp_stores(spark)))
-            .groupBy("product_id")
-            .agg(F.countDistinct("store_id").alias("n_stores"))
-            )
-
-def second_most_selling_store_per_product(spark):
-    ticket_lines_file_path = "ticket_line.csv"
-    sorted_quantities = Window.partitionBy("product_id").orderBy(F.col('quantity').desc())
-    return (ticket_lines(spark, ticket_lines_file_path)
-            .filter(F.col("store_id").isin(lp_stores(spark)))
-            .groupBy("product_id", "store_id")
-            .agg(F.sum("quantity").alias("quantity"))
-            .withColumn("second_quantity", F.collect_list(F.col("quantity")).over(sorted_quantities)[1])
-            .filter(F.col("second_quantity")==F.col("quantity"))
-            .select("product_id", "store_id")
-            )
-
-def second_stores_by_product_category(spark):
-    products_file_path = "products.json"
-    return (second_most_selling_store_per_product(spark)
-            .join(products(spark, products_file_path), on="product_id")
-            .select("product_id", "store_id", "product_name", F.explode("categories").alias("category"))
-            .groupBy("category.category_name")
-            .agg(F.collect_set("store_id").alias("stores"))
-            )
-
-def main():
-    n_stores_per_product(spark).printSchema()
-    n_stores_per_product(spark).show()
-    second_most_selling_store_per_product(spark).printSchema()
-    second_most_selling_store_per_product(spark).show()
-    second_stores_by_product_category(spark).printSchema()
-    second_stores_by_product_category(spark).show()
-
-
-if __name__ == "__main__":
-    main()
+    def run_transformations(self, stores: DataFrame, ticket_lines: DataFrame, products: DataFrame):
+        stores_per_product = self.total_stores_per_product(ticket_lines, stores)
+        second_store_per_product = self.second_most_selling_store_per_product(ticket_lines)
+        second_store_by_category = self.second_popular_stores_by_product_category(ticket_lines, products)
+        return stores_per_product, second_store_per_product, second_store_by_category
